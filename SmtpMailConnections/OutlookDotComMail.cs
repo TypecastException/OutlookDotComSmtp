6b8e0ae7f6f1a3cb4ac6ccc64c08fc25b96be9a8
xivSolutions <xivSolutions@gmail.com>
2013-12-20 22:53:46 -0600

    Initial Commit

---
+++ SmtpMailConnections/OutlookDotComMail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace SmtpMailConnections
+{
+    public class OutlookDotComMail
+    {
+        string _sender = "";
+        string _password = "";
+        public OutlookDotComMail(string sender, string password)
+        {
+            _sender = sender;
+            _password = password;
+        }
+
+
+        public void SendMail(string recipient, string subject, string message)
+        {
+            SmtpClient client = new SmtpClient("smtp-mail.outlook.com");
+
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(_sender, _password);
+            client.EnableSsl = true;
+            client.Credentials = credentials;
+
+            try
+            {
+                var mail = new MailMessage(_sender.Trim(), recipient.Trim());
+                mail.Subject = subject;
+                mail.Body = message;
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SmtpMailConnections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmtpMailConnections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mailUser = "YourAccount@outlook.com";
+            string mailUserPwd = "aotuptnagnkbfiez";
+
+            var sender = new OutlookDotComMail(mailUser, mailUserPwd);
+            sender.SendMail("recipient@example.com", "Test Mail", "Hello!");
+        }
+    }
+}
